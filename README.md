f6351821873a5a9c469f84167bbe40259eb61a25
nasvks <nas@vks.io>
2019-10-25 12:05:26 +0200

    Reformat conf file, modify package checks and update README.md

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # lemp-roll
-A script used to install or remove packages on Debian-based distros
+An NGINX, MySQL and PHP install and clean up script for Debian-based distros
 
 ## Usage
-Execute ``run.sh roll`` to install all packages specified in run.conf or ``run.sh unroll`` to remove them.
+Execute ``sudo bash run.sh roll`` to install packages specified in run.conf or ``sudo bash run.sh unroll`` to remove them. Additional packages can be installed by adding them to the run.conf file.
 
 ## Testing
-A Vagrantfile has been included to make testing a little easier. Install Vagrant, download the file and execute ``vagrant up``.
+A Vagrantfile has been included to make testing the script a little easier. Follow the instructions below to make use of it.
+
+1. ``sudo apt-get install vagrant`` to install Vagrant
+2. ``sudo git clone https://github.com/nasvks/lemp-roll.git`` to clone the repository
+3. ``vagrant up`` to boot the virtual machine
+4. ``vagrant ssh`` to login to the virtual machine
+5. ``sudo bash /vagrant/run.sh roll`` to install packages
+
+## Todo 
+* Secure access to the virtual machine using ufw
+* Install Let's Encrypt to enable HTTPS for the web service
+* Install the latest copy of Wordpress
+* Figure out how best to configure NGINX, MySQL and PHP for Wordpress
+* Create functions where applicable and refactor
+
+## Disclaimer
+
+This is the first Git repository that I created after completing the Atlassian [Version Control with Git](https://www.coursera.org/learn/version-control-with-git) course on Coursera. Drop me an [email](mailto:nas@vks.io) if you'd like to suggest an improvement to the way in which I've used Github.
--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-# Description: Script to install or remove packages on Debian-based distros
+# Description: An NGINX, MySQL and PHP install and clean up script for Debian-based distros
 #      Author: Nasso Vikos <nas@vks.io>
 
 user=$(whoami)
 filename=$(basename "$0" .sh)
+config="$filename.conf"
 log="$filename.log"
+ip=$(ip route get 1 | head -n 1 | awk '{ print $7 }')
+url="http://$ip:8080"
 
 echo ""
 
@@ -24,12 +27,12 @@ if [[ $1 == "roll" ]]; then
   if [[ $? -eq 0 ]]; then
     echo -e "[  OK  ] Package index has been updated.\\n"
   else
-    echo -e "[ FAIL ] Package index update failed. Review $log for details.\\n"
+    echo -e "[ EXIT ] Package index update failed. Review $log for details.\\n"
     exit 1
   fi
 
-  # Check if NGINX, MySQL and PHP installed else install
-  for package in $(cat run.conf); do
+  # Check whether packages are installed else install
+  for package in $(cat "$config" | grep install | cut -d "=" -f 1); do
   package_status=$(dpkg-query --show --showformat='${db:Status-Status}' "$package" 2>> "$log" )
     if [[ $? -eq 0 ]] && [[ $package_status == "installed" ]]; then
       echo -e "[  OK  ] Package $package is installed.\\n"
@@ -40,19 +43,54 @@ if [[ $1 == "roll" ]]; then
       if [[ $? -eq 0 ]]; then
         echo -e "[  OK  ] Package $package has been installed.\\n"
       else
-        echo -e "[ FAIL ] Package installation failed. Review $log for details.\\n"
+        echo -e "[ EXIT ] Package installation failed. Review $log for details.\\n"
         exit 1
       fi
     else
-      echo -e "[ FAIL ] Error querying package status. Review $log for details.\\n"
+      echo -e "[ EXIT ] Error querying package status. Review $log for details.\\n"
       exit 1
     fi
   done
 
+  # Check if NGINX and MySQL are running
+  systemctl is-active mysql &>> /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo -e "[  OK  ] Service MySQL is active.\\n"
+  else
+    echo -e "[ WAIT ] Service MySQL is inactive. Starting...\\n"
+    systemctl start mysql &>> "$log"
+    if [[ $? -eq 0 ]]; then
+      echo -e "[  OK  ] Service MySQL is active.\\n"
+    else
+      echo -e "[ EXIT ] Service MySQL is inactive. Try to start it manually.\\n"
+      exit 1
+    fi
+  fi
+
+  systemctl is-active nginx &>> /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo -e "[  OK  ] Service NGINX is active.\\n"
+    if [[ $(logname) == "vagrant" ]]; then
+      echo -e "[ DONE ] Access the web service at http://127.0.0.1:8080.\\n"
+    else
+      echo -e "[ DONE ] Access the web service at $url.\\n"
+    fi
+  else
+    echo -e "[ WAIT ] Service NGINX is inactive. Starting...\\n"
+    systemctl start nginx &>> "$log"
+    if [[ $? -eq 0 ]]; then
+      echo -e "[  OK  ] Service NGINX is active.\\n"
+      echo -e "[ DONE ] Access the web service at $url or http://127.0.0.1:8080.\\n"
+    else
+      echo -e "[ EXIT ] Service NGINX is inactive. Try to start it manually.\\n"
+      exit 1
+    fi
+  fi
+
 elif [[ $1 == "unroll" ]]; then
 
-  # Check if NGINX, MySQL and PHP installed and remove or skip
-  for package in $(tac run.conf); do
+  # Check whether packages are installed and remove or skip
+  for package in $(tac "$config" | grep install | cut -d "=" -f 1); do
     package_status=$(dpkg-query --show --showformat='${db:Status-Status}' "$package" 2>> "$log" )
     if [[ $? -eq 0 ]] && [[ $package_status == "installed" ]]; then
       echo -e "[  OK  ] Package $package is installed. Removing...\\n"
@@ -60,13 +98,13 @@ elif [[ $1 == "unroll" ]]; then
       if [[ $? -eq 0 ]]; then
         echo -e "[  OK  ] Package $package has been removed.\\n"
       else
-        echo -e "[ FAIL ] Package removal failed. Review $log for details.\\n"
+        echo -e "[ EXIT ] Package removal failed. Review $log for details.\\n"
         exit 1
       fi
     elif [[ $? -eq 1 ]]; then
       echo -e "[ WAIT ] Package $package is not installed. Skipping...\\n"
     else
-      echo -e "[ FAIL ] Error querying package status. Review $log for details.\\n"
+      echo -e "[ EXIT ] Error querying package status. Review $log for details.\\n"
       exit 1
     fi
   done
@@ -77,7 +115,7 @@ elif [[ $1 == "unroll" ]]; then
   if [[ $? -eq 0 ]]; then
     echo -e "[  OK  ] Package dependencies have been removed.\\n"
   else
-    echo -e "[ FAIL ] Package dependencies clean up failed. Review $log for details.\\n"
+    echo -e "[ EXIT ] Package dependencies clean up failed. Review $log for details.\\n"
     exit 1
   fi
 
