16fdb6159c0766f5fb2a7d19ea5eb1ae341d787b
nasvks <nas@vks.io>
2019-11-08 12:24:39 +0200

    run.sh: Add graceful script termination feature

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 # Description: An NGINX, MySQL and PHP install and clean up script for Debian-based distros
 #      Author: Nasso Vikos <nas@vks.io>
 
+stty -echo # Disable keyboard input echoing
+
 user=$(whoami)
 filename=$(basename "$0" .sh)
 config="$filename.conf"
@@ -15,8 +17,14 @@ echo ""
 # Check whether user has permission to install packages
 if [[ $user != "root" ]]; then
   echo -e "[ EXIT ] This script must be run as root.\\n"
-  exit 1
+  stty echo; exit 1
 fi
+
+# Catch Ctrl-C key press
+echo -e "[ INFO ] Hit Ctrl-C to break and quit (exit 2).\\n"
+trap 'echo -e "[ EXIT ] Script terminated by user.\\n";
+      stty echo;
+      exit 2' INT
 
 # Check command line parameter
 if [[ $1 == "roll" ]]; then
@@ -32,7 +40,7 @@ if [[ $1 == "roll" ]]; then
       echo -e "[  OK  ] Firewall configuration completed.\\n"
     else
       echo -e "[ EXIT ] Firewall configuration failed. Review $log for details.\\n"
-      exit 1
+      stty echo; exit 1
     fi
   else
     echo -e "[ INFO ] Skipping firewall configuration.\\n"
@@ -44,7 +52,7 @@ if [[ $1 == "roll" ]]; then
     echo -e "[  OK  ] Internet connectivity confirmed.\\n"
   else
     echo -e "[ EXIT ] Internet connectivity test failed. Check network connectivity.\\n"
-    exit 1
+    stty echo; exit 1
   fi
 
   # Update the package index
@@ -54,7 +62,7 @@ if [[ $1 == "roll" ]]; then
     echo -e "[  OK  ] Package index has been updated.\\n"
   else
     echo -e "[ EXIT ] Package index update failed. Review $log for details.\\n"
-    exit 1
+    stty echo; exit 1
   fi
 
   # Check whether packages are installed else install
@@ -70,11 +78,11 @@ if [[ $1 == "roll" ]]; then
         echo -e "[  OK  ] Package $package has been installed.\\n"
       else
         echo -e "[ EXIT ] Package installation failed. Review $log for details.\\n"
-        exit 1
+        stty echo; exit 1
       fi
     else
       echo -e "[ EXIT ] Error querying package status. Review $log for details.\\n"
-      exit 1
+      stty echo; exit 1
     fi
   done
 
@@ -89,7 +97,7 @@ if [[ $1 == "roll" ]]; then
       echo -e "[  OK  ] Service MySQL is active.\\n"
     else
       echo -e "[ EXIT ] Service start failed. Try to start it manually.\\n"
-      exit 1
+      stty echo; exit 1
     fi
   fi
 
@@ -113,7 +121,7 @@ if [[ $1 == "roll" ]]; then
       fi
     else
       echo -e "[ EXIT ] Service start failed. Try to start it manually.\\n"
-      exit 1
+      stty echo; exit 1
     fi
   fi
 
@@ -129,13 +137,13 @@ elif [[ $1 == "unroll" ]]; then
         echo -e "[  OK  ] Package $package has been removed.\\n"
       else
         echo -e "[ EXIT ] Package removal failed. Review $log for details.\\n"
-        exit 1
+        stty echo; exit 1
       fi
     elif [[ $? -eq 1 ]]; then
       echo -e "[ INFO ] Package $package is not installed. Skipping...\\n"
     else
       echo -e "[ EXIT ] Error querying package status. Review $log for details.\\n"
-      exit 1
+      stty echo; exit 1
     fi
   done
 
@@ -146,7 +154,7 @@ elif [[ $1 == "unroll" ]]; then
     echo -e "[  OK  ] Package dependencies have been removed.\\n"
   else
     echo -e "[ EXIT ] Package dependencies clean up failed. Review $log for details.\\n"
-    exit 1
+    stty echo; exit 1
   fi
 
   # Deconfigure firewall
@@ -156,17 +164,19 @@ elif [[ $1 == "unroll" ]]; then
     echo -e "[  OK  ] Firewall has been deconfigured.\\n"
   else
     echo -e "[ EXIT ] Firewall could not be deconfigured. Review $log for details.\\n"
-    exit 1
+    stty echo; exit 1
   fi
 
 else
 
   # Exit and display help
   echo -e "[ EXIT ] Execute 'run.sh roll' to install all packages in run.conf or 'run.sh unroll' to remove them.\\n"
-  exit 1
+  stty echo; exit 1
 
 fi
 
 echo -e "[ DONE ] All tasks completed.\\n"
+
+stty echo # Enable keyboard input echoing
 
 exit 0
